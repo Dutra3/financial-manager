2bac83c06a010a0acbcabbe1bc19839e4bad448c
gabriel <gabrieldutra45@gmail.com>
2024-05-19 03:30:15 -0300

    Add header component to wallet page

---
+++ src/pages/Wallet/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header";
+import "./Wallet.css";
+
+const Wallet = () => {
+    return (
+        <>
+            <Header title="Olá Gabriel, voce esta na aba de Wallet, aqui você encontra as suas acoes e fundos imobilidados cadastros, podendo tambem mudar a quantidade de cada um deles."/>
+            <div>
+                
+            </div>
+        </>
+    )
+};
+
+export { Wallet };
