72a43ad106f3bab9a5e556e0e4e51e350afa28ec
gabriel <gabrieldutra45@gmail.com>
2024-05-19 03:13:01 -0300

    Add header component to profile page

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header";
+import "./Profile.css";
+
+const Profile = () => {
+    return (
+        <>
+            <Header title="Olá Gabriel, voce esta na aba de Profile, aqui você encontra os seus dados pessoais, como nome, salario e metas."/>
+            <div>
+                
+            </div>
+        </>
+    )
+};
+
+export { Profile };
