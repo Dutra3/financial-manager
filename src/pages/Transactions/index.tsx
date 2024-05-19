0eacc87da8cccfc2118c50ef20916a84e95fd13e
gabriel <gabrieldutra45@gmail.com>
2024-05-19 03:30:04 -0300

    Add header component to transactions page

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header";
+import "./Transactions.css";
+
+const Transactions = () => {
+    return (
+        <>
+            <Header title="Olá Gabriel, voce esta na aba de Transactions, aqui você encontra as suas despesas e receitas passadas, como o cadastro de novos dados de despesas e receitas."/>
+            <div>
+                
+            </div>
+        </>
+    )
+};
+
+export { Transactions };
