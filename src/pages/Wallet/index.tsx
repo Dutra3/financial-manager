9e1def9a78996c80ac9fd145e7b38f8d29beb722
Dutra3 <gabrieldutra45@gmail.com>
2024-05-29 00:19:58 -0300

    Add css configuration to wallet page

--- src/pages/Wallet/index.tsx
+++ src/pages/Wallet/index.tsx
@@ -4,13 +4,15 @@ import "./Wallet.css";
 
 const Wallet = () => {
     return (
-        <>
-            <Header title="Olá, Gabriel. Você está na aba de Carteira. Aqui você encontra suas ações e fundos imobiliários cadastrados, podendo também alterar a quantidade de cada um deles."/>
+        <main className="home-container">
             <SideBar page="wallet"/>
-            <div>
-                <h1>Wallet Page</h1>
+            <div className="main-content">
+                <Header title="Olá, Gabriel. Você está na aba de Carteira. Aqui você encontra suas ações e fundos imobiliários cadastrados, podendo também alterar a quantidade de cada um deles."/>
+                <div>
+                    <h1>Wallet Page</h1>
+                </div>
             </div>
-        </>
+        </main>
     )
 };
 
