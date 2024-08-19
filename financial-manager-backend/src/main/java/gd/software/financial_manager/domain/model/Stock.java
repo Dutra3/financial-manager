6e189a7c2be4e1e2e6c578cbc8ea063728af848e
gabriel <gabrieldutra45@gmail.com>
2024-08-18 21:40:08 -0300

    Create stock class

---
+++ financial-manager-backend/src/main/java/gd/software/financial_manager/domain/model/Stock.java
@@ -0,0 +1,59 @@
+package gd.software.financial_manager.domain.model;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Stock {
+
+    private UUID id;
+    private String name;
+    private String description;
+    private BigDecimal quantity;
+    private BigDecimal averagePrice;
+    private BigDecimal priceToBookRatio;
+    private BigDecimal dividend;
+    private BigDecimal dividendYield;
+
+    public Stock(UUID id, String name, String description, BigDecimal quantity, BigDecimal averagePrice, BigDecimal priceToBookRatio, BigDecimal dividend, BigDecimal dividendYield) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.averagePrice = averagePrice;
+        this.priceToBookRatio = priceToBookRatio;
+        this.dividend = dividend;
+        this.dividendYield = dividendYield;
+    }
+
+    public UUID id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public BigDecimal quantity() {
+        return quantity;
+    }
+
+    public BigDecimal averagePrice() {
+        return averagePrice;
+    }
+
+    public BigDecimal priceToBookRatio() {
+        return priceToBookRatio;
+    }
+
+    public BigDecimal dividend() {
+        return dividend;
+    }
+
+    public BigDecimal dividendYield() {
+        return dividendYield;
+    }
+}
