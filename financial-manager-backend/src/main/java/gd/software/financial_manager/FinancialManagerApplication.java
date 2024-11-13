fdfcf321b408d0646c46e509a29a148486e34e7a
gabriel <gabrieldutra45@gmail.com>
2024-11-13 02:10:32 -0300

    Config DB connection

--- financial-manager-backend/src/main/java/gd/software/financial_manager/FinancialManagerApplication.java
+++ financial-manager-backend/src/main/java/gd/software/financial_manager/FinancialManagerApplication.java
@@ -1,12 +1,21 @@
 package gd.software.financial_manager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class FinancialManagerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+		System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+		System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
+
 		SpringApplication.run(FinancialManagerApplication.class, args);
 	}
 
