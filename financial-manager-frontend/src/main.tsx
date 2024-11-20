31e5f9924f3cd93e8fae1cbd1a5256761476a0af
Gabriel Dutra <gabrieldutra45@gmail.com>
2024-11-20 13:15:26 -0300

    Change class name to Route to better fit with the folder

--- financial-manager-frontend/src/main.tsx
+++ financial-manager-frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes } from './routes';
+import { Routes } from './routes/Routes';
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
+	<React.StrictMode>
+    	<Routes />
+  	</React.StrictMode>
 )
