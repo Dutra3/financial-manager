540e2f4957aa3d9e5704cd57365ff9686eec30e4
Gabriel Dutra <gabrieldutra45@gmail.com>
2024-11-21 01:29:38 -0300

    Add design to forgot password page

--- financial-manager-frontend/src/pages/ForgotPassword/index.tsx
+++ financial-manager-frontend/src/pages/ForgotPassword/index.tsx
@@ -1,16 +1,30 @@
-
+import { useState } from "react";
+import './ForgotPassword.css';
 
 const ForgotPassword: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+
+    const handleForgotPassword = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        console.log(`Send recovery email to: ${email}`);
+    }
+
     return (
-        <div>
-            <h2>Forgot my password</h2>
-            <form>
-                <div className="form-group">
-                    <label>Email:</label>
-                    <input type="email" placeholder="Digite seu email" />
-                </div>
-                <button type="submit">Recovery password</button>
-            </form>
+        <div className="forgot-password-container">
+            <div className="forgot-password-data">
+                <h2>Forgot my password</h2>
+                <form className="forgot-password-form" onSubmit={handleForgotPassword}>
+                    <div className="forgot-form-group">
+                        <label>Email:</label>
+                        <input 
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)} 
+                            placeholder="Write your email"
+                            required />
+                    </div>
+                    <button className="recovery-button" type="submit">Recovery password</button>
+                </form>
+            </div>
         </div>
     );
 };
