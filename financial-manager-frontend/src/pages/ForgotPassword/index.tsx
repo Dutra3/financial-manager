9feb8dcf95b446c94de58425dff904a05ed64344
Gabriel Dutra <gabrieldutra45@gmail.com>
2024-11-21 00:22:16 -0300

    Create forgot password page

--- financial-manager-frontend/src/pages/ForgotPassword/index.tsx
+++ financial-manager-frontend/src/pages/ForgotPassword/index.tsx
@@ -1,8 +1,17 @@
 
 
-const ForgotPassword = () => {
+const ForgotPassword: React.FC = () => {
     return (
-        <div><p>Forgot Password</p></div>
+        <div>
+            <h2>Forgot my password</h2>
+            <form>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="email" placeholder="Digite seu email" />
+                </div>
+                <button type="submit">Recovery password</button>
+            </form>
+        </div>
     );
 };
 
