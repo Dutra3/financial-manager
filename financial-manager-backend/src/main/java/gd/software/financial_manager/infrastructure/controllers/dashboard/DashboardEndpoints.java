1009c72aa5e38102ba97e9592618c90d9d4d4d22
Gabriel Dutra <gabrieldutra45@gmail.com>
2024-12-03 23:56:52 -0300

    Create example of an endpoint in dashboard

--- financial-manager-backend/src/main/java/gd/software/financial_manager/infrastructure/controllers/dashboard/DashboardEndpoints.java
+++ financial-manager-backend/src/main/java/gd/software/financial_manager/infrastructure/controllers/dashboard/DashboardEndpoints.java
@@ -1,9 +1,21 @@
 package gd.software.financial_manager.infrastructure.controllers.dashboard;
 
+import gd.software.financial_manager.infrastructure.dtos.MonthlyDebit;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/dashboards")
 public class DashboardEndpoints {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MonthlyDebit> fetchAllDebits(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 }
---
+++ financial-manager-backend/src/main/java/gd/software/financial_manager/infrastructure/dtos/MonthlyDebit.java
@@ -0,0 +1,7 @@
+package gd.software.financial_manager.infrastructure.dtos;
+
+import java.math.BigDecimal;
+
+public record MonthlyDebit(
+        BigDecimal amount
+) {}
