c7927e60962cb6d69215609379ef5252828be28b
Gabriel Dutra <gabrieldutra45@gmail.com>
2024-12-08 11:50:57 -0300

    Create function get all debits in dashboard page

--- financial-manager-frontend/src/pages/Home/index.tsx
+++ financial-manager-frontend/src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
 import { Card } from "../../components/Card";
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/SideBar";
+import { getDebits } from "../../api/dashboardApi";
 import "./Home.css";
 
 const Home = () => {
+
+    const getAllDebits = async () => {
+        const debits = await getDebits();
+        console.log(debits);
+    }
+
     return (
         <main className="home-container">
             <SideBar page="dashboard"/> 
@@ -14,6 +21,11 @@ const Home = () => {
                 <Card title="Expenses" amount={2500.00} />
                 <div className="dashboard-content">
                     <h1>Dashboard Page</h1>
+                </div>
+                <div>
+                    <button onClick={getAllDebits}>
+                        Get all debits.
+                    </button>
                 </div>
             </div>
         </main>
