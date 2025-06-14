d3f068f00d037304a19e6101cc033ec034f2f763
Gabriel Dutra <gabrieldutra45@gmail.com>
2025-06-14 20:44:21 -0300

    Create log in walletendpoints

--- financial-manager-backend/src/main/java/gd/software/financial_manager/infrastructure/controllers/wallet/WalletEndpoints.java
+++ financial-manager-backend/src/main/java/gd/software/financial_manager/infrastructure/controllers/wallet/WalletEndpoints.java
@@ -1,9 +1,21 @@
 package gd.software.financial_manager.infrastructure.controllers.wallet;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/wallets")
 public class WalletEndpoints {
+
+    private static final Logger logger = LoggerFactory.getLogger(WalletEndpoints.class);
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WalletResponse> fetchWallet(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 }
